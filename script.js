2b168514b5de41b29d52829c9a3d27780b242f48
Mukesh Mali <67068171+mukesh001731@users.noreply.github.com>
2024-02-27 23:57:48 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// JS Code Complete it till 20th Feb 2024  Mukesh,
-// Get the elements
 const form = document.getElementById('search-form');
 const cityInput = document.getElementById('city-input');
 const button = document.getElementById('location-button');
@@ -63,49 +61,3 @@ function success(position) {
 function error() {
   weatherData.innerHTML = 'Unable to retrieve your location';
 }
-
-
-
-
-
-
-
-
-
-// Complelete it ASAP Mukesh, To start creating Content from 22nd Jan
- // document.getElementById('search-btn').addEventListener('click', function() {
- //  const city = document.getElementById('city').value;
-
- //  if (city === '') {
- //       alert('Please enter a city name or "current" for current location.');
- //       return;
- //   }
-
- //   fetchWeather(city);
- // });
-
- // function fetchWeather(city) {
- //   const apiKey = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
- //   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
- //   fetch(apiUrl)
- //       .then(response => response.json())
- //       .then(data => displayWeather(data))
- //       .catch(error => console.error('Error:', error));
- // }
-
- // function displayWeather(data) {
- //   const { name } = data;
- //   const { temp, humidity } = data.main;
- //   const { description, icon } = data.weather[0];
-
- //   const weatherResults = `
- //       <h2>${name}</h2>
- //       <h3>${temp}°C</h3>
- //       <h4>${description}</h4>
- //       <h4>Humidity: ${humidity}%</h4>
- //       <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="${description}">
- //   `;
-
- //   document.getElementById('weather-results').innerHTML = weatherResults;
- // }
